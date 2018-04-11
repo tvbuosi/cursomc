5e0e0d1b8e38e3a952ebe54ecb741678de41b41f
Thiago Vermelho Buosi <thiago_buosi@hotmail.com>
2018-04-11 01:08:38 -0300

    Estado e cidade

--- src/main/java/com/thiagobuosi/cursomc/services/CategoriaService.java
+++ src/main/java/com/thiagobuosi/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.thiagobuosi.cursomc.domain.Categoria;
 import com.thiagobuosi.cursomc.repositories.CategoriaRepository;
+import com.thiagobuosi.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -17,7 +18,9 @@ public class CategoriaService {
 	public Categoria buscar(Integer id) {
 		
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null); 
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 		
 	}
 	
