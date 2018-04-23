58d4a96ec9ea2574fb5627286611cceaf8794dbb
Thiago Vermelho Buosi <thiago_buosi@hotmail.com>
2018-04-23 00:20:05 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/thiagobuosi/cursomc/services/PedidoService.java
@@ -0,0 +1,27 @@
+package com.thiagobuosi.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thiagobuosi.cursomc.domain.Pedido;
+import com.thiagobuosi.cursomc.repositories.PedidoRepository;
+import com.thiagobuosi.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		
+		Optional<Pedido> obj = repo.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+		
+	}
+	
+}
