98b34e78c35018a0949f1a7ab85c434a8e7b04f7
Thiago Vermelho Buosi <thiago_buosi@hotmail.com>
2018-05-06 18:45:29 -0300

    Atualizando categoria com PUT

--- src/main/java/com/thiagobuosi/cursomc/services/CategoriaService.java
+++ src/main/java/com/thiagobuosi/cursomc/services/CategoriaService.java
@@ -15,7 +15,7 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		
 		Optional<Categoria> obj = repo.findById(id);
 		
@@ -29,5 +29,10 @@ public class CategoriaService {
 		obj.setId(null);
 		return repo.save(obj);
 		
+	}
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
 	}
 }
