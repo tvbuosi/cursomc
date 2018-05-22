1bc9cfa4631583fa00779494d46e28a4c831916d
Thiago Vermelho Buosi <thiago_buosi@hotmail.com>
2018-05-21 22:45:54 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/thiagobuosi/cursomc/services/ClienteService.java
+++ src/main/java/com/thiagobuosi/cursomc/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.thiagobuosi.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.thiagobuosi.cursomc.domain.Cliente;
+import com.thiagobuosi.cursomc.dto.ClienteDTO;
 import com.thiagobuosi.cursomc.repositories.ClienteRepository;
+import com.thiagobuosi.cursomc.services.exceptions.DataIntegrityException;
 import com.thiagobuosi.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -21,6 +28,56 @@ public class ClienteService {
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+		
+	}
+	
+	public Cliente update(Cliente obj) {
+		
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+		
+	}
+	
+	public void delete(Integer id) {
+		
+		find(id);
+		
+		try {
+			
+			repo.deleteById(id);
+			
+		} catch (DataIntegrityViolationException e) {
+			
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
+			
+		}
+		
+	}
+	
+	public List<Cliente> findAll(){
+		
+		return repo.findAll();
+		
+	}
+	
+	//PAGINAÇÃO
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		return repo.findAll(pageRequest);		
+	}
+	
+	//Transforma DTO em Obj
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 		
 	}
 	
