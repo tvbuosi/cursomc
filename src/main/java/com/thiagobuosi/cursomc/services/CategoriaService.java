c87be40ef61edece1c495b0cc06a2dde859613a6
Thiago Vermelho Buosi <thiago_buosi@hotmail.com>
2018-05-31 15:19:46 -0300

    Inserindo um novo cliente com POST

--- src/main/java/com/thiagobuosi/cursomc/services/CategoriaService.java
+++ src/main/java/com/thiagobuosi/cursomc/services/CategoriaService.java
@@ -39,8 +39,9 @@ public class CategoriaService {
 	
 	public Categoria update(Categoria obj) {
 		
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 		
 	}
 	
@@ -77,6 +78,12 @@ public class CategoriaService {
 	//Transforma DTO em Obj
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		
+		newObj.setNome(obj.getNome());
+		
 	}
 	
 }
--- src/main/java/com/thiagobuosi/cursomc/services/ClienteService.java
+++ src/main/java/com/thiagobuosi/cursomc/services/ClienteService.java
@@ -3,6 +3,8 @@ package com.thiagobuosi.cursomc.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -10,9 +12,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.thiagobuosi.cursomc.domain.Cidade;
 import com.thiagobuosi.cursomc.domain.Cliente;
+import com.thiagobuosi.cursomc.domain.Endereco;
+import com.thiagobuosi.cursomc.domain.enums.TipoCliente;
 import com.thiagobuosi.cursomc.dto.ClienteDTO;
+import com.thiagobuosi.cursomc.dto.ClienteNewDTO;
 import com.thiagobuosi.cursomc.repositories.ClienteRepository;
+import com.thiagobuosi.cursomc.repositories.EnderecoRepository;
 import com.thiagobuosi.cursomc.services.exceptions.DataIntegrityException;
 import com.thiagobuosi.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -22,12 +29,26 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
 	public Cliente find(Integer id) {
 		
 		Optional<Cliente> obj = repo.findById(id);
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+		
+	}
+	
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		
+		return obj;
 		
 	}
 	
@@ -49,7 +70,7 @@ public class ClienteService {
 			
 		} catch (DataIntegrityViolationException e) {
 			
-			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
+			throw new DataIntegrityException("Não é possível excluir um cliente que possui pedidos.");
 			
 		}
 		
@@ -72,6 +93,31 @@ public class ClienteService {
 	//Transforma DTO em Obj
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		//Criando novo cliente
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		
+		//Cidade do endereço
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		
+		//Criando novo endereço
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(),
+				cli, cid);
+		cli.getEnderecos().add(end);
+		
+		//Criando telefones
+		cli.getTelefones().add(objDto.getTelefone1());
+		//Caso tenha cadastrado telefone 2 e 3
+		if(objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if(objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		
+		return cli;
 	}
 	
 	private void updateData(Cliente newObj, Cliente obj) {
