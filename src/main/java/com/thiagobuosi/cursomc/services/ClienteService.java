30e007eaadbb88a0a4eff6444782d7f8d7c1ad1c
CSC-SJRP\tbuosi2 <tbuosi@dxc.com>
2018-09-19 14:18:47 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/thiagobuosi/cursomc/services/ClienteService.java
+++ src/main/java/com/thiagobuosi/cursomc/services/ClienteService.java
@@ -41,7 +41,7 @@ public class ClienteService {
 	public Cliente find(Integer id) {
 		UserSS user = UserService.authenticated();
 		if(user==null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
-			throw new AuthorizationException("Acesso negado");
+			throw new AuthorizationException("Acesso negado");  
 		}
 		
 		Optional<Cliente> obj = repo.findById(id);
--- src/main/java/com/thiagobuosi/cursomc/services/PedidoService.java
+++ src/main/java/com/thiagobuosi/cursomc/services/PedidoService.java
@@ -4,8 +4,12 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.thiagobuosi.cursomc.domain.Cliente;
 import com.thiagobuosi.cursomc.domain.ItemPedido;
 import com.thiagobuosi.cursomc.domain.PagamentoComBoleto;
 import com.thiagobuosi.cursomc.domain.Pedido;
@@ -13,6 +17,8 @@ import com.thiagobuosi.cursomc.domain.enums.EstadoPagamento;
 import com.thiagobuosi.cursomc.repositories.ItemPedidoRepository;
 import com.thiagobuosi.cursomc.repositories.PagamentoRepository;
 import com.thiagobuosi.cursomc.repositories.PedidoRepository;
+import com.thiagobuosi.cursomc.security.UserSS;
+import com.thiagobuosi.cursomc.services.exceptions.AuthorizationException;
 import com.thiagobuosi.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -66,5 +72,19 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		
+		UserSS user = UserService.authenticated();
+		
+		if(user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 }
