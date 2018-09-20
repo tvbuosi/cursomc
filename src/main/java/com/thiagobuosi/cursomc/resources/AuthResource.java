9e83cd3f4359f8b605e979dfa5fca76429927468
CSC-SJRP\tbuosi2 <tbuosi@dxc.com>
2018-09-20 16:24:32 -0300

    Esqueci a senha

--- src/main/java/com/thiagobuosi/cursomc/resources/AuthResource.java
+++ src/main/java/com/thiagobuosi/cursomc/resources/AuthResource.java
@@ -1,23 +1,30 @@
 package com.thiagobuosi.cursomc.resources;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.thiagobuosi.cursomc.dto.EmailDTO;
 import com.thiagobuosi.cursomc.security.JWTUtil;
 import com.thiagobuosi.cursomc.security.UserSS;
+import com.thiagobuosi.cursomc.services.AuthService;
 import com.thiagobuosi.cursomc.services.UserService;
 
 @RestController
 @RequestMapping(value="/auth")
 public class AuthResource {
-	
+	 
 	@Autowired
 	private JWTUtil jwtUtil;
+	
+	@Autowired
+	private AuthService authService;
 	
 	@RequestMapping(value="/refresh_token", method=RequestMethod.POST)
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
@@ -25,6 +32,15 @@ public class AuthResource {
 		UserSS user = UserService.authenticated();
 		String token = jwtUtil.generateToken(user.getUsername());
 		response.addHeader("Authorization", "Bearer " + token);
+		
+		return ResponseEntity.noContent().build();
+		
+	}
+	
+	@RequestMapping(value="/forgot", method=RequestMethod.POST)
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDTO) {
+		
+		authService.sendNewPassword(objDTO.getEmail());
 		
 		return ResponseEntity.noContent().build();
 		
--- src/main/java/com/thiagobuosi/cursomc/services/EmailService.java
+++ src/main/java/com/thiagobuosi/cursomc/services/EmailService.java
@@ -2,6 +2,7 @@ package com.thiagobuosi.cursomc.services;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.thiagobuosi.cursomc.domain.Cliente;
 import com.thiagobuosi.cursomc.domain.Pedido;
 
 public interface EmailService {
@@ -9,4 +10,7 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
+	
 }
