ac544c0af7b184c9074b1911f248960a9842e78e
CSC-SJRP\tbuosi2 <tbuosi@dxc.com>
2018-09-26 10:34:19 -0300

    Endpoint para buscar cliente por email

--- src/main/java/com/thiagobuosi/cursomc/services/ClienteService.java
+++ src/main/java/com/thiagobuosi/cursomc/services/ClienteService.java
@@ -51,7 +51,7 @@ public class ClienteService {
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
 	
-	@Value("${img.profile.size")
+	@Value("${img.profile.size}")
 	private Integer size;
 	
 	public Cliente find(Integer id) {
@@ -91,6 +91,23 @@ public class ClienteService {
 	
 	public List<Cliente> findAll() {
 		return repo.findAll();
+	}
+	
+	public Cliente findByEmail(String email) {
+		
+		UserSS user = UserService.authenticated();
+		if(user==null || !user.hasRole(Perfil.ADMIN) && !email.equals(user.getUsername())) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
+		Cliente obj = repo.findByEmail(email);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + user.getId()
+					+ ", Tipo: " + Cliente.class.getName());
+		}
+		
+		return obj;		
+		
 	}
 	
 	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
